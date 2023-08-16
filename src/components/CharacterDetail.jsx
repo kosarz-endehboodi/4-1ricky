04b7745ad42592d6c78879d10b7d1d67396fd4f9
kosarz-endehboodi <imkosarzbdm@gmail.com>
2023-08-16 10:30:56 +0330

    added ui characterdetail

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,16 +1,57 @@
-import { character } from "../../data/data";
-
+import { character, episodes } from "../../data/data";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 export default function CharacterDetail() {
   return (
     <div style={{ flex: 1 }}>
       <div className="character-detail">
-        <img className="character-detail__img" src={character.image} alt={character.name} />
+        <img
+          className="character-detail__img"
+          src={character.image}
+          alt={character.name}
+        />
         <div className="character-detail__info">
-        <span>{character.gender === "Male" ? "🧑" : "👩"}</span>
-         <h3 className="name">{character.name}</h3>
+          <h3>
+            <span>{character.gender === "Male" ? "🧑" : "👩"}</span>
+            <span className="name">{character.name}</span>
+          </h3>
+          <div className=" info">
+            <span
+              className={`status ${character.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span>&nbsp; {character.status}</span>
+            <span>&nbsp; {character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last know Location:</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add to Favorite</button>
+          </div>
         </div>
       </div>
-      <div className="character-episodes">2</div>
+      <div className="character-episodes">
+        <div className="title">
+          <h2>List of Episodes</h2>
+          <button>
+            <ArrowUpCircleIcon className="icon" />
+          </button>
+        </div>
+        <ul>
+          {episodes.map((item, index) => {
+            return (
+              <li key={item.id}>
+                <div>
+              
+                  {String(index + 1).padStart(2, "0")}
+                 - {item.episode} : <strong>{item.name}</strong>
+                </div>
+                <div className="badge badge--secondary">{item.air_date}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
