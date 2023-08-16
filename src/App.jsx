1b0333052f6c4f9540526562d013c5b83e171260
kosarz-endehboodi <imkosarzbdm@gmail.com>
2023-08-16 17:48:48 +0330

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
+import { useState } from "react";
 export default function App() {
+  const [characters, setcharacter] = useState(allCharacters);
   return (
-    <div className="app">
-      <div>
-        <NavBar />
-      </div>
-      <div className="main">
-        <CharacterList Characters={allCharacters} />
-        <CharacterDetail />
+    <div className="container">
+      <div className="app">
+        <div>
+          <NavBar characterResult={characters.length}/>
+        </div>
+        <div className="main" >
+          <CharacterList Characters={allCharacters} />
+          <CharacterDetail />
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
-export default function NavBar() {
+export default function NavBar({ characterResult }) {
   return (
-    <div className="navbar">
-      <div className="navbar-logo">logo:|</div>
-      <input type="search" placeholder="search" className="text-field" />
-      <div className="navbar__result">fund and result</div>
-      <div className="heart">
-        <HeartIcon className="icon" />
-        <span className="badge">4</span>
+   
+      <div className="navbar">
+        <div className="navbar-logo">logo:|</div>
+        <input type="search" placeholder="search" className="text-field" />
+        <div className="navbar__result">found {characterResult} result</div>
+        <div className="heart">
+          <HeartIcon className="icon" />
+          <span className="badge">4</span>
+        </div>
       </div>
-    </div>
+  
   );
 }
